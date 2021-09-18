6b0c6528649537972f44f662f01e70a1664fcb2c
Stefan Schindler <stefs@posteo.de>
2021-09-18 12:51:27 +0200

    Progress width based on duration

--- happy_day.py
+++ happy_day.py
@@ -4,8 +4,10 @@ import logging
 import sys
 from dataclasses import dataclass
 from datetime import time, datetime, timedelta
+from typing import Optional
 
 from PySide6.QtCore import QTimer
+from PySide6.QtGui import QResizeEvent
 from PySide6.QtWidgets import QMainWindow, QApplication, QGridLayout, QLabel, QProgressBar, QWidget
 
 
@@ -25,10 +27,16 @@ class Phase(object):
     start: time
 
 
+@dataclass
+class Progress(object):
+    value: float
+    total: float
+
+
 class HappyDay(QMainWindow):
     PHASES = [
-        Phase('Wake', time(hour=7)),
-        Phase('Sleep', time(hour=23))]
+        Phase(name='Wake', start=time(hour=7)),
+        Phase(name='Sleep', start=time(hour=23))]
 
     def __init__(self) -> None:
         super().__init__()
@@ -43,23 +51,53 @@ class HappyDay(QMainWindow):
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
+        # attributes
+        self.progress_values: Optional[list[Progress]] = None
+        self.progress_width_offset: Optional[int] = None
         # start status update
+        QTimer.singleShot(100, self.update_status)
         self.timer = QTimer()
-        self.timer.setInterval(1000)
+        self.timer.setInterval(10000)
         self.timer.timeout.connect(self.update_status)
         self.timer.start()
 
+    def resizeEvent(
+            self,
+            event: QResizeEvent
+    ) -> None:
+        self.update_progress_width(event.oldSize().width(), event.size().width())
+        super().resizeEvent(event)
+
+    def update_progress_width(
+            self,
+            width_old: int,
+            width_new: int
+    ) -> None:
+        if self.progress_values is None:
+            return
+        if self.progress_width_offset is None:
+            self.progress_width_offset = width_old - self.progress_bars[0].width()
+        width_max = width_new - self.progress_width_offset
+        total_max = max(progress_value.total for progress_value in self.progress_values)
+        for progress_bar, progress in zip(self.progress_bars, self.progress_values):
+            progress_bar.setMaximumWidth(progress.total / total_max * width_max)
+
     def update_status(self) -> None:
         now = datetime.now()
-        start_times = [
-            datetime.combine(now.date(), phase.start)
-            for phase in self.PHASES]
+        # calculate start times
+        start_times = [datetime.combine(now.date(), phase.start) for phase in self.PHASES]
         start_times.append(datetime.combine(now.date() + timedelta(days=1), self.PHASES[0].start))
-        for index, progress_bar in enumerate(self.progress_bars):
-            start_time = start_times[index]
-            start_time_next = start_times[index + 1]
-            progress_bar.setMaximum((start_time_next - start_time).total_seconds())
-            progress_bar.setValue((now - start_time).total_seconds())
+        self.progress_values = [
+            Progress(
+                value=(now - start_times[index]).total_seconds(),
+                total=(start_times[index + 1] - start_times[index]).total_seconds())
+            for index in range(len(self.progress_bars))]
+        # set progress bars values
+        for progress_bar, progress in zip(self.progress_bars, self.progress_values):
+            progress_bar.setMaximum(progress.total)
+            progress_bar.setValue(progress.value)
+        # set progress bar widths
+        self.update_progress_width(self.width(), self.width())
 
 
 if __name__ == '__main__':
